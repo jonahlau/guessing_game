efc65704d8319ffbbd529eff7ab541c7223eb707
Jonah Lau <j.nh.lau@gmail.com>
2014-07-21 21:48:50 +0800

    added enter key input

--- js/application.js
+++ js/application.js
@@ -2,56 +2,64 @@ $(document).ready(function() {
 	var count = 5;
 	var randomNum = Math.round(Math.random() * 100);
 
-	$("#go-button").on("click", function() {
-	
-		var userInput = $("#form-input").val();
+	var gameFunction = function() {	
+		var userInput = +$("#form-input").val();
 		var para1 = $(".para1").last("p");	
+			$(".para1").find(".nodeMarker").remove();
 
-		$(".para1").find(".nodeMarker").remove();
+			count--;
 
-		count--;
+			$("#countdown").text(count);
 
-		$("#countdown").text(count);
-
-		if (userInput == randomNum) {
-			para1.append("<p class=\"nodeMarker\">You've guessed the right number! You win!</p>");
-			$(".guess").append("<li class =\"superclose\">" + userInput + "</li>");
-			$("#go-button").hide();
-			$("#form-input").hide();
-			$("#hint-button").hide();
-		 } else if ((userInput <= randomNum + 5 && userInput >= randomNum - 5) && userInput > randomNum) {
-			para1.append("<p class=\"nodeMarker\">You're getting super hot! Guess lower!</p>");
-			$(".guess").append("<li class = \"superclose\">" + userInput + "</li>");
-		} else if ((userInput <= randomNum + 5 && userInput >= randomNum - 5) && userInput < randomNum) {
-				para1.append("<p class=\"nodeMarker\">You're getting super hot! Guess higher!</p>");
-			$(".guess").append("<li class = \"superclose\">" + userInput + "</li>");
-		} else if ((userInput <= randomNum + 10 && userInput >= randomNum - 10) && userInput > randomNum) {
-			para1.append("<p class=\"nodeMarker\">You're getting close! Guess lower!</p>");
-			$(".guess").append("<li class = \"close\">" + userInput + "</li>");
-		} else if ((userInput <= randomNum + 10 && userInput >= randomNum - 10) && userInput < randomNum) {
-			para1.append("<p class=\"nodeMarker\">You're getting close! Guess higher!</p>");
-			$(".guess").append("<li class=\"close\">" + userInput + "</li>");
-		} else if ((userInput <= randomNum + 20 && userInput >= randomNum - 20) && userInput > randomNum) {
-			para1.append("<p class=\"nodeMarker\">You're quite far off. Guess lower.</p>");
-			$(".guess").append("<li class=\"cold\">" + userInput + "</li>");
-		} else if ((userInput <= randomNum + 20 && userInput >= randomNum - 20) && userInput > randomNum) {
-			para1.append("<p class=\"nodeMarker\">You're quite far off. Guess higher.</p>");
-			$(".guess").append("<li class=\"cold\">" + userInput + "</li>");
-		} else {
-			if (count == 0) {
-				$(".guess").append("<li class=\"icecold\">" + userInput + "</li>");
-				$(".para1").find(".nodeMarker").remove();
-				para1.append("<p class=\"nodeMarker\">You've run out of chances. You lose!</p>");
+			if (userInput == randomNum) {
+				para1.append("<p class=\"nodeMarker\">You've guessed the right number! You win!</p>");
+				$(".guess").append("<li class =\"superclose\">" + userInput + "</li>");
 				$("#go-button").hide();
 				$("#form-input").hide();
-				$(".hint").hide();
-			} else {
-			para1.append("<p class=\"nodeMarker\">You're no where near the right answer. Guess another number.</p>");
-			$(".guess").append("<li class=\"icecold\">" + userInput + "</li>");
+				$("#hint-button").hide();
+			 } else if ((userInput <= randomNum + 5 && userInput >= randomNum - 5) && userInput > randomNum) {
+				para1.append("<p class=\"nodeMarker\">You're getting super hot! Guess lower!</p>");
+				$(".guess").append("<li class = \"superclose\">" + userInput + "</li>");
+			} else if ((userInput <= randomNum + 5 && userInput >= randomNum - 5) && userInput < randomNum) {
+					para1.append("<p class=\"nodeMarker\">You're getting super hot! Guess higher!</p>");
+				$(".guess").append("<li class = \"superclose\">" + userInput + "</li>");
+			} else if ((userInput <= randomNum + 10 && userInput >= randomNum - 10) && userInput > randomNum) {
+				para1.append("<p class=\"nodeMarker\">You're getting close! Guess lower!</p>");
+				$(".guess").append("<li class = \"close\">" + userInput + "</li>");
+			} else if ((userInput <= randomNum + 10 && userInput >= randomNum - 10) && userInput < randomNum) {
+				para1.append("<p class=\"nodeMarker\">You're getting close! Guess higher!</p>");
+				$(".guess").append("<li class=\"close\">" + userInput + "</li>");
+			} else if ((userInput <= randomNum + 20 && userInput >= randomNum - 20) && userInput > randomNum) {
+				para1.append("<p class=\"nodeMarker\">You're quite far off. Guess lower.</p>");
+				$(".guess").append("<li class=\"cold\">" + userInput + "</li>");
+			} else if ((userInput <= randomNum + 20 && userInput >= randomNum - 20) && userInput > randomNum) {
+				para1.append("<p class=\"nodeMarker\">You're quite far off. Guess higher.</p>");
+				$(".guess").append("<li class=\"cold\">" + userInput + "</li>");
+			}	else {
+				if (count == 0) {
+					$(".guess").append("<li class=\"icecold\">" + userInput + "</li>");
+					$(".para1").find(".nodeMarker").remove();
+					para1.append("<p class=\"nodeMarker\">You've run out of chances. You lose!</p>");
+					$("#go-button").hide();
+					$("#form-input").hide();
+					$(".hint").hide();
+				} else {
+				para1.append("<p class=\"nodeMarker\">You're no where near the right answer. Guess another number.</p>");
+				$(".guess").append("<li class=\"icecold\">" + userInput + "</li>");
+				}
 			}
-		};
-
 		$("#form-input").val("");
+	};
+
+	$("#go-button").on("click", function() {
+		return gameFunction();
+	});
+
+	$("#form-input").on("keypress", function(e) {
+		var userInput = +$("#form-input").val();
+		if (e.keyCode==13) {
+				return gameFunction();
+			}
 	});
 
 	$("#hint-button").on("click", function() {
